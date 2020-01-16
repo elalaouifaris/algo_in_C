83844351f0ea770bd7b2833a4401605d2337bbc8
elalaouifaris <elalaouifaris@gmail.com>
2020-01-16 22:44:49 +0100

    Implement quicksort

---
+++ sorting/quicksort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <criterion/criterion.h>
+
+#include "sorting_utils.h"
+
+void quicksort(int *input, size_t size);
+void _quicksort(int *input, size_t lower_bound, size_t upper_bound);
+size_t __partition(int *input, size_t lower_bound, size_t upper_bound);
+
+void quicksort(int *input, size_t size) {
+	_quicksort(input, 0, size - 1);
+}
+
+void _quicksort(int *input, size_t lower_bound, size_t upper_bound) {
+	if (upper_bound <= lower_bound) {
+		return;
+	}
+
+	size_t pivot_location = __partition(input, lower_bound, upper_bound);
+	_quicksort(input, lower_bound, pivot_location - 1);
+	_quicksort(input, pivot_location + 1, upper_bound);
+}
+
+size_t __partition(int *input, size_t lower_bound, size_t upper_bound) {
+	int pivot = input[lower_bound];
+	size_t start = lower_bound + 1;
+	size_t end = upper_bound;
+
+	while (start <= end) {
+
+		while (input[start] <= pivot) {
+			start++;
+		}
+		while (input[end] > pivot) {
+			end--;
+		}
+
+		if (start < end) {
+			swap(input, start, end);
+		}
+	}
+
+	swap(input, lower_bound, end);
+	return end;
+}
+
+Test(quicksort, simple_case) {
+	int input[5] = {2, 4, 5, 4, 1};
+	int expected[5] = {1, 2, 4, 4, 5};
+
+	quicksort(input, 5);
+
+	check_sorted(input, 5);
+	cr_expect_arr_eq(input, expected, 5 * sizeof(int));
+}
